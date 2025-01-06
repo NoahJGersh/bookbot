9c1577094918656efadd5e6ce4c8aa32d3f68d58
NoahJGersh <me@noahger.sh>
2025-01-05 18:58:13 -0800

    Implement get_word_count()

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+        print("Word count:", get_word_count(file_contents))
+
+def get_word_count(text):
+    return len(text.split())
 
 main()
